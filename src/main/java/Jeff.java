abbb508ac86eb6aa5aabaf35d01f29881f7e7e59
vet3whale <githubvetri@gmail.com>
2026-01-24 08:15:50 +0800

    Rename Duke to Jeff

---
+++ src/main/java/Jeff.java
@@ -0,0 +1,12 @@
+public class Jeff {
+    public static void main(String[] args) {
+        String chatbotName = "Jeff";
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm " + chatbotName);
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
