a05fd3338941e55d3848340e088ac23e130a2ec7
vet3whale <githubvetri@gmail.com>
2026-01-28 17:51:11 +0800

    Add echo functionality for Jeff

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Jeff {
+    public static void receiveInput() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        String response = in.nextLine();
+        while (!response.equals("bye")) {
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t " + response);
+            System.out.println("\t____________________________________________________________");
+            response = in.nextLine();
+        }
+    }
     public static void main(String[] args) {
         String chatbotName = "Jeff";
 
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm " + chatbotName);
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t Hello! myname" + chatbotName);
+        System.out.println("\t What can I do for you?");
+        System.out.println("\t____________________________________________________________");
+
+        receiveInput();
+
+        System.out.println("\t Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
