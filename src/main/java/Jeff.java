10acc89fa5a3be59cd87a266d7819e30cd9be5e1
vet3whale <githubvetri@gmail.com>
2026-01-28 18:18:05 +0800

    Add the ability to store tasks entered by users

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -2,17 +2,34 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Jeff {
+    public static void printList(String[] tasks, int count){
+        System.out.println("\t____________________________________________________________");
+        for (int i = 0; i < count; i++) {
+            int itemNumber = i+1;
+            System.out.println("\t " + itemNumber + ". " + tasks[i]);
+        }
+        System.out.println("\t____________________________________________________________");
+    }
+
     public static void receiveInput() {
-        String line;
+        String[] tasks = new String[100];
+        int count = 0;
         Scanner in = new Scanner(System.in);
         String response = in.nextLine();
         while (!response.equals("bye")) {
-            System.out.println("\t____________________________________________________________");
-            System.out.println("\t " + response);
-            System.out.println("\t____________________________________________________________");
+            if (response.equals("list")) {
+                printList(tasks, count);
+            } else {
+                tasks[count] = response;
+                count++;
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\t added: " + response);
+                System.out.println("\t____________________________________________________________");
+            }
             response = in.nextLine();
         }
     }
+
     public static void main(String[] args) {
         String chatbotName = "Jeff";
 
@@ -23,7 +40,8 @@ public class Jeff {
 
         receiveInput();
 
+        System.out.println("\t____________________________________________________________");
         System.out.println("\t Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
     }
 }
